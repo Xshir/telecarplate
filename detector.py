cceec1da21c172111b1d75a4a468992ed3a9918b
Xshir <Haashir150@gmail.com>
2023-05-22 15:03:37 +0800

    bug

--- detector.py
+++ detector.py
@@ -66,16 +66,16 @@ def get_detected_license_plate_number(img, todays_vip_cars):
     if detected == 1:
         cv2.drawContours(img, [screenCnt], -1, (0, 0, 255), 3)
 
-        
-        new_image = cv2.drawContours(
-            mask,
-            [screenCnt],
-            0,
-            255,
-            -1,
-        )
-        new_image = cv2.bitwise_and(img, img, mask=mask)
     mask = np.zeros(gray.shape, np.uint8)
+    new_image = cv2.drawContours(
+        mask,
+        [screenCnt],
+        0,
+        255,
+        -1,
+    )
+    new_image = cv2.bitwise_and(img, img, mask=mask)
+
     (x, y) = np.where(mask == 255)
     (topx, topy) = (np.min(x), np.min(y))
     (bottomx, bottomy) = (np.max(x), np.max(y))
